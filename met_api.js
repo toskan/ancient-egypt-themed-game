a0d68081f35e385add48949561c5a0f8d0146f17
toskan <tamar.oskanian@gmail.com>
2019-09-16 16:40:02 -0400

    met api and responsive still some bugs

--- met_api.js
+++ met_api.js
@@ -1,11 +1,11 @@
 
 
-let imageObjectIDS = [551302, 570708, 544864, 546748, 546745, 544222, 543870, 547803, 551502, 549070, 548580]
+let imageObjectIDS = [551302, 570708, 546748, 546745, 544222, 543870, 547803, 551502, 549070, 548580, 544864]
 
 let apiURL = 'https://collectionapi.metmuseum.org/public/collection/v1/objects/';
 
 let urls = [];
-
+let count = 0;
 
 imageObjectIDS.forEach(function(i, j){
      urls[j] = apiURL + i;
@@ -22,7 +22,6 @@ Promise.all(urls.map(url =>
           //this returns the info so it can be worked with
           return response.json();
      }
-     
      let elevenImages = [];
      let objectName = [];
      let objectDate = [];
@@ -39,7 +38,9 @@ Promise.all(urls.map(url =>
           console.log(objectName);
           console.log(objectDate);
           console.log(objectMedium);
-          console.log(objectURL); 
+          console.log(objectURL);
+          $('.slides-content').prepend('<h2 class="object-name">' + objectName[count] + '<h2><img class="eleven-images" src=' + elevenImages[count] + '><h3 class="object-date">' + objectDate[count] + '</h3><h3 class="object-medium">' + objectMedium[count] + '</h3><ul class="met-link-ul"><li class="met-link-li"><a class="met-link-a" href=' + objectURL[count]  + '>More info at the Metropolitan Museum</a></li></ul>');
+          count++;
      }
 
      function addToSearch(dataItems) {
