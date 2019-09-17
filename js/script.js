ebae6cf1932f3652eee1137f5f131c242c4c92ee
toskan <tamar.oskanian@gmail.com>
2019-09-17 12:26:56 -0400

    added modal

--- js/script.js
+++ js/script.js
@@ -20,6 +20,7 @@ function onReady () {
           './images/img8_20.2.1.jpeg', 
           './images/img9_DP-14816-003.jpg'
      ]
+     let matches = 0;
 
      let array18 = [];
      for (var i = 0; i < 18; i ++) {
@@ -82,7 +83,7 @@ function onReady () {
      $('.flip-card-inner').each(function(j){
           $(this).on("click", {x:j}, function(event) {
                storedEventData.push(event.data.x);
-               $(this).toggleClass('flip', true);
+               $(this).toggleClass('flip', true).promise.then 
                // $(innerCard[storedEventData[0]]).off(event);
                x++;
           if (x >= 2) {
@@ -96,6 +97,11 @@ function onReady () {
                if (imageArray[storedEventData[0]] === (imageArray[storedEventData[1]])) {
                     x = 0;
                     storedEventData = [];
+                    matches++
+                    alert(matches)
+                    if (matches === 1) {
+                         $('#modal-slides').css("display", "block");
+                    }  
                }
                else {
                     $(innerCard[storedEventData[0]]).toggleClass('flip', false);
@@ -103,11 +109,67 @@ function onReady () {
                     storedEventData = [];
                     x = 0;
                }    
-          }, 2500);  
-     }    
+          }, 2500);
+     }
      
      console.log(storedEventData)
-     
+
+     $('.button-display-topright').click(closeModal);
+     function closeModal() {
+          $('#modal-slides').css("display", "none");
+     }
+
+     let imageObjectIDS = [551302, 546748, 544222, 543870, 547803, 551502, 549070, 548580, 544864, 570708, 546745]
+
+let apiURL = 'https://collectionapi.metmuseum.org/public/collection/v1/objects/';
+
+let urls = [];
+let count = 0;
+
+imageObjectIDS.forEach(function(i, j){
+     urls[j] = apiURL + i;
+});
+console.log(urls)
+
+Promise.all(urls.map(url =>
+     fetch(url)
+     .then(convertToJson)
+     .then(useData)
+)) 
+
+     function convertToJson(response) {
+          //this returns the info so it can be worked with
+          return response.json();
+     }
+     let elevenImages = [];
+     let objectName = [];
+     let objectDate = [];
+     let objectMedium = [];
+     let objectURL = [];
+     function useData (jsonData) {
+          console.log(jsonData)
+          elevenImages[elevenImages.length] = jsonData.primaryImageSmall;
+          objectName[objectName.length] = jsonData.objectName;
+          objectMedium[objectMedium.length] = jsonData.medium;
+          objectDate[objectDate.length] = jsonData.objectDate;
+          objectURL[objectURL.length] = jsonData.objectURL;
+          $('.slides-content').prepend('<div class="slides-div"><h2 class="object-name">' + objectName[count] + '<h2><img class="eleven-images" src=' + elevenImages[count] + '><h3 class="object-date">' + objectDate[count] + '</h3><h3 class="object-medium">' + objectMedium[count] + '</h3><ul class="met-link-ul"><li class="met-link-li"><a class="met-link-a" href=' + objectURL[count]  + '>More info at the Metropolitan Museum</a></li></ul></div>');
+          count++;
+     }
+
+     // target="_blank" 
+     $('.met-link-a').attr('target', '_blank');
+
+     function addToSearch(dataItems) {
+          fetch(dataItems)
+          .then(convertToJson)
+          .then(applyData);
+     }
+
+     function applyData(dataJson) {
+          console.log(dataJson);
+     }
+
      var isChrome = /Chrome/.test(navigator.userAgent) && /Google Inc/.test(navigator.vendor);
      if (isChrome) {
           $('.flip-card-bottom, .flip-card-top').css({'backface-visibility': 'visible', ' -webkit-backface-visibility': 'visible'});
--- met_api.js
+++ met_api.js
@@ -1,6 +1,6 @@
 
 
-let imageObjectIDS = [551302, 570708, 546748, 546745, 544222, 543870, 547803, 551502, 549070, 548580, 544864]
+let imageObjectIDS = [551302, 546748, 544222, 543870, 547803, 551502, 549070, 548580, 544864, 570708, 546745]
 
 let apiURL = 'https://collectionapi.metmuseum.org/public/collection/v1/objects/';
 
@@ -39,7 +39,7 @@ Promise.all(urls.map(url =>
           console.log(objectDate);
           console.log(objectMedium);
           console.log(objectURL);
-          $('.slides-content').prepend('<h2 class="object-name">' + objectName[count] + '<h2><img class="eleven-images" src=' + elevenImages[count] + '><h3 class="object-date">' + objectDate[count] + '</h3><h3 class="object-medium">' + objectMedium[count] + '</h3><ul class="met-link-ul"><li class="met-link-li"><a class="met-link-a" href=' + objectURL[count]  + '>More info at the Metropolitan Museum</a></li></ul>');
+          $('.slides-content').prepend('<div class="slides-div"><h2 class="object-name">' + objectName[count] + '<h2><img class="eleven-images" src=' + elevenImages[count] + '><h3 class="object-date">' + objectDate[count] + '</h3><h3 class="object-medium">' + objectMedium[count] + '</h3><ul class="met-link-ul"><li class="met-link-li"><a class="met-link-a" href=' + objectURL[count]  + '>More info at the Metropolitan Museum</a></li></ul></div>');
           count++;
      }
 
@@ -52,3 +52,25 @@ Promise.all(urls.map(url =>
      function applyData(dataJson) {
           console.log(dataJson);
      }
+
+     // <button class="display-left" onclick="plusDivs(-1)">&#10094;</button>
+     // <button class="display-right" onclick="plusDivs(+1)">&#10095;</button>
+
+
+// let slideIndex = 1;
+// showDivs(slideIndex);
+
+// function plusDivs(n) {
+//      showDivs(slideIndex += n);
+// }
+
+// function showDivs(n) {
+//      let x = $(".slides-div");
+//      if (n > x.length) {slideIndex = 1}
+//      if (n < 1) {slideIndex = x.length} ;
+//      for (let i = 0; i < x.length; i++) {
+//      $(x[i]).css("display", "none");
+//      }
+//      $(x[slideIndex-1]).css("display", "block");
+// }
+
