ddbac9590fbcfb11b0c7d765995fe95f2774c418
toskan <tamar.oskanian@gmail.com>
2019-09-18 08:59:22 -0400

    mobile logic partly fixed

---
+++ js/mobile.js
@@ -0,0 +1,208 @@
+let mobilePortrait = window.matchMedia ( "((max-width: 414px)  and (orientation: portrait))" )
+let mobileLandscape = window.matchMedia ("(max-width: 823px) and (orientation: landscape)");
+
+if ((mobilePortrait.matches) || (mobileLandscape.matches)) {
+     console.log(document.readyState);
+     const images = [
+          './images/img1_DP243443.jpg', 
+          './images/img2_DP240268.jpg', 
+          './images/img3_DP245119.jpg', 
+          './images/img4_DP245144.jpg', 
+          './images/img5_26.7.1416_01.jpg', 
+          './images/img6_DP-17700-001.jpg',  
+          './images/img1_DP243443.jpg', 
+          './images/img2_DP240268.jpg', 
+          './images/img3_DP245119.jpg', 
+          './images/img4_DP245144.jpg', 
+          './images/img5_26.7.1416_01.jpg',
+          './images/img6_DP-17700-001.jpg',
+     ]
+     console.log(images)
+     let matches = 0;
+
+     let array12 = [];
+     for (var i = 0; i < 12; i ++) {
+          array12[i] = i;
+     }
+     function shuffleArray(array) {
+          for (let i = array.length - 1; i > 0; i--) {
+               const j = Math.floor(Math.random() * (i + 1));
+               [array[i], array[j]] = [array[j], array[i]];
+               }
+          }
+     shuffleArray(array12);
+     console.log(array12);
+
+     //  The Fisher-Yates algorithm works by picking one random element for each original array element, and then excluding it from the next draw. Just like randomly picking from a deck of cards.
+     //  This exclusion is done in a clever way (invented by Durstenfeld for use by computers) by swapping the picked element with the current element, and then picking the next random element from the remainder. For optimal efficiency, the loop runs backwards so that the random pick is simplified
+
+     let imageArray = []
+     for (let i = 0; i < array12.length; i++) {
+          imageArray.splice(array12[i], 0, images[i]);
+     }
+     console.log(imageArray);
+     let imagesContainer = []
+     for (let i = 0; i < 18; i++) {
+          imagesContainer[i] = i;
+          imagesContainer[i] = '#image-' + imagesContainer[i];
+     }
+     imagesContainer.splice(4, 2);
+     imagesContainer.splice(8, 2);
+     imagesContainer.splice(12, 2);
+     console.log(imagesContainer);
+     let innerCard = []
+     for (let i = 0; i < 17; i++) {
+          innerCard[i] = i;
+          innerCard[i] = '#flip-card-inner-' + innerCard[i];
+     }
+     innerCard.splice(4, 2);
+     innerCard.splice(8, 2);
+     innerCard.splice(12, 2);
+     console.log(innerCard);
+     $('body').css('height', window.innerHeight); 
+
+     $(window).resize(function() {
+          $('body').css('height', window.innerHeight); 
+     });
+
+     $('#play').click(startPlay);
+     function startPlay() { 
+          if (document.readyState === "complete") { 
+               $('#header-play-div').hide();
+               $('.container').show();
+               $('body').css('background-image', 'none'); 
+               for (let i = 0; i < imagesContainer.length; i++) {
+                    $(imagesContainer[i]).attr('src', imageArray[i]);
+               }
+          }
+          if (document.readyState !== "complete") {
+                    $('.container, .loader-container').show();
+                    $('#header-play-div, #row1, #row2, #row3').hide(); 
+               }
+     }
+
+     let storedEventData = []
+     let x = 0;
+
+     $('.flip-card-inner').each(function(j){
+          $(this).on("click", {x:j}, function(event) {
+               storedEventData.push(event.data.x);
+               $(this).toggleClass('flip', true).promise.then 
+               // $(innerCard[storedEventData[0]]).off(event);
+               x++;
+          if (x >= 2) {
+               compareImages();
+               }
+          });
+     });
+     
+     function compareImages() {
+          setTimeout(function() {
+               if (imageArray[storedEventData[0]] === (imageArray[storedEventData[1]])) {
+                    x = 0;
+                    storedEventData = [];
+                    matches++
+                    alert(matches)
+                    if (matches === 9) {
+                         $('#win-restart').css("display", "block");
+                    }  
+               }
+               else {
+                    $(innerCard[storedEventData[0]]).toggleClass('flip', false);
+                    $(innerCard[storedEventData[1]]).toggleClass('flip', false);
+                    storedEventData = [];
+                    x = 0;
+               }    
+          }, 2500);
+     }
+     
+     console.log(storedEventData)
+
+     $('.button-win-restart-topright').click(closeModal);
+     function closeModal() {
+          $('#win-restart').css("display", "none");
+          location.reload();
+     }
+
+     $('#play-again').click(playAgain);
+     function playAgain() {
+          $('#win-restart').css("display", "none");
+          location.reload();
+     }
+
+     $('#art-info').click(artInfo);
+     function artInfo() {
+          $('#win-restart').css("display", "none");
+          $('#modal-slides').css("display", "block");
+     }
+
+     $('.button-display-topright').click(closeArtModal);
+     function closeArtModal() {
+          $('#modal-slides').css("display", "none");
+          location.reload();
+     }
+
+     let imageObjectIDS = [551302, 544222, 547803, 551502, 549070, 548580, 544864, 546745];
+
+let apiURL = 'https://collectionapi.metmuseum.org/public/collection/v1/objects/';
+
+let urls = [];
+let count = 0;
+
+imageObjectIDS.forEach(function(i, j){
+     urls[j] = apiURL + i;
+});
+console.log(urls)
+
+Promise.all(urls.map(url =>
+     fetch(url)
+     .then(convertToJson)
+     .then(useData)
+)) 
+
+     function convertToJson(response) {
+          //this returns the info so it can be worked with
+          return response.json();
+     }
+     let elevenImages = [];
+     let objectName = [];
+     let objectCountry = []
+     let objectDate = [];
+     let objectMedium = [];
+     let objectURL = [];
+     function useData (jsonData) {
+          console.log(jsonData)
+          elevenImages[elevenImages.length] = jsonData.primaryImageSmall;
+          objectName[objectName.length] = jsonData.objectName;
+          objectCountry[objectCountry.length] = jsonData.country;
+          objectMedium[objectMedium.length] = jsonData.medium;
+          objectDate[objectDate.length] = jsonData.objectDate;
+          objectURL[objectURL.length] = jsonData.objectURL;
+          $('.slides-content').prepend('<div class="slides-div"><h2 class="object-name">' + objectName[count] + '<h2><img class="eleven-images" src=' + elevenImages[count] + '><h3 class="object-country">' + objectCountry[count] + '</h3><h3 class="object-date">' + objectDate[count] + '</h3><h3 class="object-medium">' + objectMedium[count] + '</h3><ul class="met-link-ul"><li class="met-link-li"><a class="met-link-a" href=' + objectURL[count]  + '>More info at the Metropolitan Museum</a></li></ul></div>');
+          count++;
+          $('.met-link-a').attr('target', '_blank');
+     }
+
+     function addToSearch(dataItems) {
+          fetch(dataItems)
+          .then(convertToJson)
+          .then(applyData);
+     }
+
+     function applyData(dataJson) {
+          console.log(dataJson);
+     }
+
+     var isChrome = /Chrome/.test(navigator.userAgent) && /Google Inc/.test(navigator.vendor);
+     if (isChrome) {
+          $('.flip-card-bottom, .flip-card-top').css({'backface-visibility': 'visible', ' -webkit-backface-visibility': 'visible'});
+     } 
+
+}
+
+
+
+
+
+
+
--- js/script.js
+++ js/script.js
@@ -1,5 +1,212 @@
 function onReady () {
      console.log(document.readyState);
+     //NOT SURE THIS WORKS
+     
+     if  ((((window.matchMedia("(max-width: 414px)").matches)) && ((window.matchMedia( "(orientation: portrait)").matches))) || (((window.matchMedia("(max-width: 823px)").matches)) && ((window.matchMedia( "(orientation: landscape)").matches))))  {
+          console.log(document.readyState);
+          const images = [
+               './images/img1_DP243443.jpg', 
+               './images/img2_DP240268.jpg', 
+               './images/img3_DP245119.jpg', 
+               './images/img4_DP245144.jpg', 
+               './images/img5_26.7.1416_01.jpg', 
+               './images/img6_DP-17700-001.jpg',  
+               './images/img1_DP243443.jpg', 
+               './images/img2_DP240268.jpg', 
+               './images/img3_DP245119.jpg', 
+               './images/img4_DP245144.jpg', 
+               './images/img5_26.7.1416_01.jpg',
+               './images/img6_DP-17700-001.jpg',
+          ]
+          console.log(images)
+          let matches = 0;
+
+          let array12 = [];
+          for (var i = 0; i < 12; i ++) {
+               array12[i] = i;
+          }
+          function shuffleArray(array) {
+               for (let i = array.length - 1; i > 0; i--) {
+                    const j = Math.floor(Math.random() * (i + 1));
+                    [array[i], array[j]] = [array[j], array[i]];
+                    }
+               }
+          shuffleArray(array12);
+          console.log(array12);
+
+          let imageArray = []
+          for (let i = 0; i < array12.length; i++) {
+               imageArray.splice(array12[i], 0, images[i]);
+          }
+          console.log(imageArray);
+          let imagesContainer = []
+          for (let i = 0; i < 18; i++) {
+               imagesContainer[i] = i;
+               imagesContainer[i] = '#image-' + imagesContainer[i];
+          }
+          imagesContainer.splice(4, 2);
+          imagesContainer.splice(8, 2);
+          imagesContainer.splice(12, 2);
+          console.log(imagesContainer);
+          let innerCard = []
+          for (let i = 0; i < 17; i++) {
+               innerCard[i] = i;
+               innerCard[i] = '#flip-card-inner-' + innerCard[i];
+          }
+          innerCard.splice(4, 2);
+          innerCard.splice(8, 2);
+          innerCard.splice(12, 2);
+          console.log(innerCard);
+          $('body').css('height', window.innerHeight); 
+
+          $(window).resize(function() {
+               $('body').css('height', window.innerHeight); 
+          });
+
+          $('#play').click(startPlay);
+          function startPlay() { 
+               if (document.readyState === "complete") { 
+                    $('#header-play-div').hide();
+                    $('.container').show();
+                    $('body').css('background-image', 'none'); 
+                    for (let i = 0; i < imagesContainer.length; i++) {
+                         $(imagesContainer[i]).attr('src', imageArray[i]);
+                    }
+               }
+               if (document.readyState !== "complete") {
+                         $('.container, .loader-container').show();
+                         $('#header-play-div, #row1, #row2, #row3').hide(); 
+                    }
+          }
+
+          let storedEventData = []
+          let x = 0;
+
+          $('.flip-card-inner').each(function(j){
+               $(this).on("click", {x:j}, function(event) {
+                    storedEventData.push(event.data.x);
+                    $(this).toggleClass('flip', true).promise.then
+                    alert(event.data.x + " event data")
+                    // $(innerCard[storedEventData[0]]).off(event);
+                    x++;
+               if (x >= 2) {
+                    compareImages();
+                    }
+               });
+          });
+          //because I'm going off event data, the conditionals are thrown off. The arrays depend on there being 18 items to match within the array.
+          function compareImages() {
+               setTimeout(function() {
+                         let event1 = storedEventData[0];
+                         let event2 = storedEventData[1];
+                         let innerCard1 = "#flip-card-inner-" + event1;
+                         let innerCard2 =  "#flip-card-inner-" + event2;
+                    if (imageArray[storedEventData[0]] === (imageArray[storedEventData[1]])) {
+                         x = 0;
+                         alert((imageArray[storedEventData[0]]) + " imageArray [storedEventData[0]")
+                         alert((imageArray[storedEventData[1]]) + " imageArray [storedEventData[1]")
+                         storedEventData = [];
+                         matches++
+                         if (matches === 9) {
+                              $('#win-restart').css("display", "block");
+                         }  
+                    }
+                    else {
+                         $(innerCard1).toggleClass('flip', false);
+                         $(innerCard2).toggleClass('flip', false);
+                         alert((imageArray[storedEventData[0]]) + " imageArray [storedEventData[0]")
+                         alert((imageArray[storedEventData[1]]) + " imageArray [storedEventData[1]")
+                         storedEventData = [];
+                         x = 0;
+                    }    
+               }, 2500);
+          }
+          
+          console.log(storedEventData)
+
+          $('.button-win-restart-topright').click(closeModal);
+          function closeModal() {
+               $('#win-restart').css("display", "none");
+               location.reload();
+          }
+
+          $('#play-again').click(playAgain);
+          function playAgain() {
+               $('#win-restart').css("display", "none");
+               location.reload();
+          }
+
+          $('#art-info').click(artInfo);
+          function artInfo() {
+               $('#win-restart').css("display", "none");
+               $('#modal-slides').css("display", "block");
+          }
+
+          $('.button-display-topright').click(closeArtModal);
+          function closeArtModal() {
+               $('#modal-slides').css("display", "none");
+               location.reload();
+          }
+
+          let imageObjectIDS = [551302, 544222, 547803, 551502, 549070, 548580, 544864, 546745];
+
+     let apiURL = 'https://collectionapi.metmuseum.org/public/collection/v1/objects/';
+
+     let urls = [];
+     let count = 0;
+
+     imageObjectIDS.forEach(function(i, j){
+          urls[j] = apiURL + i;
+     });
+     console.log(urls)
+
+     Promise.all(urls.map(url =>
+          fetch(url)
+          .then(convertToJson)
+          .then(useData)
+     )) 
+
+          function convertToJson(response) {
+               //this returns the info so it can be worked with
+               return response.json();
+          }
+          let elevenImages = [];
+          let objectName = [];
+          let objectCountry = []
+          let objectDate = [];
+          let objectMedium = [];
+          let objectURL = [];
+          function useData (jsonData) {
+               console.log(jsonData)
+               elevenImages[elevenImages.length] = jsonData.primaryImageSmall;
+               objectName[objectName.length] = jsonData.objectName;
+               objectCountry[objectCountry.length] = jsonData.country;
+               objectMedium[objectMedium.length] = jsonData.medium;
+               objectDate[objectDate.length] = jsonData.objectDate;
+               objectURL[objectURL.length] = jsonData.objectURL;
+               $('.slides-content').prepend('<div class="slides-div"><h2 class="object-name">' + objectName[count] + '<h2><img class="eleven-images" src=' + elevenImages[count] + '><h3 class="object-country">' + objectCountry[count] + '</h3><h3 class="object-date">' + objectDate[count] + '</h3><h3 class="object-medium">' + objectMedium[count] + '</h3><ul class="met-link-ul"><li class="met-link-li"><a class="met-link-a" href=' + objectURL[count]  + '>More info at the Metropolitan Museum</a></li></ul></div>');
+               count++;
+               $('.met-link-a').attr('target', '_blank');
+          }
+
+          function addToSearch(dataItems) {
+               fetch(dataItems)
+               .then(convertToJson)
+               .then(applyData);
+          }
+
+          function applyData(dataJson) {
+               console.log(dataJson);
+          }
+
+          var isChrome = /Chrome/.test(navigator.userAgent) && /Google Inc/.test(navigator.vendor);
+          if (isChrome) {
+               $('.flip-card-bottom, .flip-card-top').css({'backface-visibility': 'visible', ' -webkit-backface-visibility': 'visible'});
+          } 
+
+     }
+     //UNSURE REMOVE IF MOBILE SECTION
+     else {
      const images = [
           './images/img1_DP243443.jpg', 
           './images/img2_DP240268.jpg', 
@@ -95,8 +302,6 @@ function onReady () {
           setTimeout(function() {
                if (imageArray[storedEventData[0]] === (imageArray[storedEventData[1]])) {
                     x = 0;
-                    $(innerCard[storedEventData[0]]).addClass('bounce-win');
-                    $(innerCard[storedEventData[1]]).addClass('bounce-win');
                     storedEventData = [];
                     matches++
                     alert(matches)
@@ -193,8 +398,8 @@ Promise.all(urls.map(url =>
      var isChrome = /Chrome/.test(navigator.userAgent) && /Google Inc/.test(navigator.vendor);
      if (isChrome) {
           $('.flip-card-bottom, .flip-card-top').css({'backface-visibility': 'visible', ' -webkit-backface-visibility': 'visible'});
-     } 
-
+     }
+}
 }
 $('document').ready(onReady);
 
