6a7a70dd8a5dcf9ca0310b07cf6148db0a007dd5
toskan <tamar.oskanian@gmail.com>
2019-09-20 16:28:05 -0400

    changed slideshow for mobile and ipad not working

--- js/script.js
+++ js/script.js
@@ -1,11 +1,8 @@
 let iCanRun = true;
 
 function onReady () {
-     console.log(document.readyState);
-     //NOT SURE THIS WORKS
-     
-     if  ((((window.matchMedia("(max-width: 414px)").matches)) && ((window.matchMedia( "(orientation: portrait)").matches))) || (((window.matchMedia("(max-width: 823px)").matches)) && ((window.matchMedia( "(orientation: landscape)").matches))))  {
-          console.log(document.readyState);
+
+     if  ((((window.matchMedia('(max-width: 414px)').matches)) && ((window.matchMedia( '(orientation: portrait)').matches))) || (((window.matchMedia('(max-width: 823px)').matches)) && ((window.matchMedia( '(orientation: landscape)').matches))))  {
           const images = [
                './images/img1_DP243443.jpg', 
                './images/img2_DP240268.jpg', 
@@ -50,15 +47,9 @@ function onReady () {
           imagesContainer.splice(8, 2);
           imagesContainer.splice(12, 2);
           console.log(imagesContainer);
-          let innerCard = []
-          for (let i = 0; i < 17; i++) {
-               innerCard[i] = i;
-               innerCard[i] = '#flip-card-inner-' + innerCard[i];
-          }
-          innerCard.splice(4, 2);
-          innerCard.splice(8, 2);
-          innerCard.splice(12, 2);
-          console.log(innerCard);
+
+          //GOT RID OF INNERCARD / NOT NEEDED HERE
+
           $('body').css('height', window.innerHeight); 
 
           $(window).resize(function() {
@@ -99,8 +90,7 @@ function onReady () {
                });
           });
           
-
-          //because I'm going off event data, the conditionals are thrown off. The arrays depend on there being 18 items to match within the arrays that correspond to index-numbers throughout (associative).
+          //because I'm going off event data to match indexes, the conditionals are thrown off. The arrays depend on there being 18 items to match within the arrays that correspond to index-numbers throughout (associative). Changed it to match the IDs, not the indexes (InnerCard Array no longer useful) 
           function compareImages() {
                setTimeout(function() {
                     if (($('#image-' + storedEventData[0]).attr('src')) === ($('#image-' + storedEventData[1]).attr('src'))) {
@@ -121,7 +111,6 @@ function onReady () {
                     } 
                }, 1300);
           }
-          
           console.log(storedEventData)
 
           $('.button-win-restart-topright').click(closeModal);
@@ -136,16 +125,15 @@ function onReady () {
                location.reload();
           }
 
-     $('#art-info').click(artInfo);
-     function artInfo() {
-          $('#win-restart').css('display', 'none');
-          $('#modal-slides').css('display', 'block');
-          $('.display-right, .display-left').hide();
-          $('.slides-div').show();
-          $('.modal-content').css('background-color', 'transparent');
-          $('#row1, #row2, #row3').hide();
-
-     }
+          $('#art-info').click(artInfo);
+          function artInfo() {
+               $('#win-restart').css('display', 'none');
+               $('#modal-slides').css('display', 'block');
+               $('.display-right, .display-left').hide();
+               $('.slides-div').show();
+               $('.modal-content').css('background-color', 'transparent');
+               $('#row1, #row2, #row3').hide();
+          }
 
           $('.button-display-topright').click(closeArtModal);
           function closeArtModal() {
@@ -155,21 +143,21 @@ function onReady () {
 
           let imageObjectIDS = [551302, 544222, 547803, 551502, 549070, 548580, 544864, 546745];
 
-     let apiURL = 'https://collectionapi.metmuseum.org/public/collection/v1/objects/';
+          let apiURL = 'https://collectionapi.metmuseum.org/public/collection/v1/objects/';
 
-     let urls = [];
-     let count = 0;
+          let urls = [];
+          let count = 0;
 
-     imageObjectIDS.forEach(function(i, j){
-          urls[j] = apiURL + i;
-     });
-     console.log(urls)
+          imageObjectIDS.forEach(function(i, j){
+               urls[j] = apiURL + i;
+          });
+          console.log(urls)
 
-     Promise.all(urls.map(url =>
-          fetch(url)
-          .then(convertToJson)
-          .then(useData)
-     )) 
+          Promise.all(urls.map(url =>
+               fetch(url)
+               .then(convertToJson)
+               .then(useData)
+          )) 
 
           function convertToJson(response) {
                //this returns the info so it can be worked with
@@ -210,195 +198,203 @@ function onReady () {
           } 
 
      }
-     //UNSURE REMOVE IF MOBILE SECTION
-     else {
-     const images = [
-          './images/img1_DP243443.jpg', 
-          './images/img2_DP240268.jpg', 
-          './images/img3_DP245119.jpg', 
-          './images/img4_DP245144.jpg', 
-          './images/img5_26.7.1416_01.jpg', 
-          './images/img6_DP-17700-001.jpg', 
-          './images/img7_O.C.380_EGDP016765.jpg', 
-          './images/img8_20.2.1.jpeg',
-          './images/img9_DP-14816-003.jpg', 
-          './images/img1_DP243443.jpg', 
-          './images/img2_DP240268.jpg', 
-          './images/img3_DP245119.jpg', 
-          './images/img4_DP245144.jpg', 
-          './images/img5_26.7.1416_01.jpg', 
-          './images/img6_DP-17700-001.jpg', 
-          './images/img7_O.C.380_EGDP016765.jpg', 
-          './images/img8_20.2.1.jpeg', 
-          './images/img9_DP-14816-003.jpg'
-     ]
-     let matches = 0;
-
-     let array18 = [];
-     for (var i = 0; i < 18; i ++) {
-          array18[i] = i;
-     }
-     function shuffleArray(array) {
-          for (let i = array.length - 1; i > 0; i--) {
-               const j = Math.floor(Math.random() * (i + 1));
-               [array[i], array[j]] = [array[j], array[i]];
-               }
-          }
-     shuffleArray(array18);
-     console.log(array18);
-
-     //  The Fisher-Yates algorithm works by picking one random element for each original array element, and then excluding it from the next draw. Just like randomly picking from a deck of cards.
-     //  This exclusion is done in a clever way (invented by Durstenfeld for use by computers) by swapping the picked element with the current element, and then picking the next random element from the remainder. For optimal efficiency, the loop runs backwards so that the random pick is simplified
-
-     let imageArray = []
-     for (let i = 0; i < array18.length; i++) {
-          imageArray.splice(array18[i], 0, images[i]);
-     }
-     let imagesContainer = []
-     for (let i = 0; i < array18.length; i++) {
-          imagesContainer[i] = i;
-          imagesContainer[i] = '#image-' + imagesContainer[i];
-     }
-     console.log(imageArray);
-     let innerCard = []
-     for (let i = 0; i < array18.length; i++) {
-          innerCard[i] = i;
-          innerCard[i] = '#flip-card-inner-' + innerCard[i];
-     }
-
-     $('body').css('height', window.innerHeight); 
-
-     $(window).resize(function() {
-          $('body').css('height', window.innerHeight); 
-     });
-
-     $('#play').click(startPlay);
-     function startPlay() {
-               $('#header-play-div').hide();
-               $('.container').show();
-               $('body').css({'background': 'url(./images/lc-23_3_465_egdp029732_broken_up_pattern_crop.jpg)', '-webkit-background-size': 'cover', '-moz-background-size': 'cover', '-o-background-size': 'cover', 'background-size': 'cover'});
-               for (let i = 0; i < imagesContainer.length; i++) {
-                    $(imagesContainer[i]).attr('src', imageArray[i]);
-               }
-          }
-
-     let storedEventData = []
-     let x = 0;
-
-
-
-     $('.flip-card-inner').each(function(j){
-          $(this).on("click", {x:j}, function(event) {
-               if (iCanRun) {
-                    $(this).toggleClass('flip', true);
-                    storedEventData.push(event.data.x); 
-                    x++;
-                    if  (storedEventData[0] === storedEventData[1]) {
-                         storedEventData.splice(1, 1);
-                         x--;
-                    }
-                    if (x >= 2) {
-                         compareImages();
-                    }
-                    iCanRun = false;
-                    setTimeout(function(){
-                         iCanRun = true;
-                    }, 1100);
-               }
-          });
-     });
-
      
-     function compareImages() {
-          setTimeout(function() {
-               if (imageArray[storedEventData[0]] === (imageArray[storedEventData[1]])) {
-                    $(innerCard[storedEventData[0]]).off();
-                    $(innerCard[storedEventData[1]]).off();
-                    x = 0;
-                    storedEventData = [];
-                    matches++;  
-                    if (matches === 9) {
-                         $('#win-restart').css("display", "block");
-                    }  
+          else {
+          const images = [
+               './images/img1_DP243443.jpg', 
+               './images/img2_DP240268.jpg', 
+               './images/img3_DP245119.jpg', 
+               './images/img4_DP245144.jpg', 
+               './images/img5_26.7.1416_01.jpg', 
+               './images/img6_DP-17700-001.jpg', 
+               './images/img7_O.C.380_EGDP016765.jpg', 
+               './images/img8_20.2.1.jpeg',
+               './images/img9_DP-14816-003.jpg', 
+               './images/img1_DP243443.jpg', 
+               './images/img2_DP240268.jpg', 
+               './images/img3_DP245119.jpg', 
+               './images/img4_DP245144.jpg', 
+               './images/img5_26.7.1416_01.jpg', 
+               './images/img6_DP-17700-001.jpg', 
+               './images/img7_O.C.380_EGDP016765.jpg', 
+               './images/img8_20.2.1.jpeg', 
+               './images/img9_DP-14816-003.jpg'
+          ]
+          let matches = 0;
+
+          let array18 = [];
+          for (var i = 0; i < 18; i ++) {
+               array18[i] = i;
+          }
+          function shuffleArray(array) {
+               for (let i = array.length - 1; i > 0; i--) {
+                    const j = Math.floor(Math.random() * (i + 1));
+                    [array[i], array[j]] = [array[j], array[i]];
+                    }
+               }
+          shuffleArray(array18);
+          console.log(array18);
+
+          //  The Fisher-Yates algorithm works by picking one random element for each original array element, and then excluding it from the next draw. Just like randomly picking from a deck of cards.
+          //  This exclusion is done in a clever way (invented by Durstenfeld for use by computers) by swapping the picked element with the current element, and then picking the next random element from the remainder. For optimal efficiency, the loop runs backwards so that the random pick is simplified
+
+          let imageArray = []
+          for (let i = 0; i < array18.length; i++) {
+               imageArray.splice(array18[i], 0, images[i]);
+          }
+          let imagesContainer = []
+          for (let i = 0; i < array18.length; i++) {
+               imagesContainer[i] = i;
+               imagesContainer[i] = '#image-' + imagesContainer[i];
+          }
+          console.log(imageArray);
+          let innerCard = []
+          for (let i = 0; i < array18.length; i++) {
+               innerCard[i] = i;
+               innerCard[i] = '#flip-card-inner-' + innerCard[i];
+          }
+
+          $('body').css('height', window.innerHeight); 
+
+          $(window).resize(function() {
+               $('body').css('height', window.innerHeight); 
+          });
+
+          $('#play').click(startPlay);
+          function startPlay() {
+                    $('#header-play-div').hide();
+                    $('.container').show();
+                    $('body').css({'background': 'url(./images/lc-23_3_465_egdp029732_broken_up_pattern_crop.jpg)', '-webkit-background-size': 'cover', '-moz-background-size': 'cover', '-o-background-size': 'cover', 'background-size': 'cover'});
+                    for (let i = 0; i < imagesContainer.length; i++) {
+                         $(imagesContainer[i]).attr('src', imageArray[i]);
+                    }
+               }
+
+          let storedEventData = []
+          let x = 0;
+
+          $('.flip-card-inner').each(function(j){
+               $(this).on("click", {x:j}, function(event) {
+                    if (iCanRun) {
+                         $(this).toggleClass('flip', true);
+                         storedEventData.push(event.data.x);
+                         x++;
+                         if  (storedEventData[0] === storedEventData[1]) {
+                              storedEventData.splice(1, 1);
+                              x--;
+                         }
+                         if (x >= 2) {
+                              compareImages();
+                         }
+                         iCanRun = false;
+                         setTimeout(function(){
+                              iCanRun = true;
+                         }, 1000);
+                    }
+               });
+          });
+     
+          function compareImages() {
+               setTimeout(function() {
+                    if (imageArray[storedEventData[0]] === (imageArray[storedEventData[1]])) {
+                         $(innerCard[storedEventData[0]]).off();
+                         $(innerCard[storedEventData[1]]).off();
+                         x = 0;
+                         storedEventData = [];
+                         matches++;  
+                         if (matches === 9) {
+                              $('#win-restart').css("display", "block");
+                         }  
+                    }
+                    else {
+                         $(innerCard[storedEventData[0]]).toggleClass('flip', false);
+                         $(innerCard[storedEventData[1]]).toggleClass('flip', false); 
+                         storedEventData = [];
+                         x = 0;
+                    }    
+               }, 1000);
+          }         
+          console.log(storedEventData);
+
+          $('.button-win-restart-topright').click(closeModal);
+          function closeModal() {
+               $('#win-restart').css("display", "none");
+               location.reload();
+          }
+
+          $('#play-again').click(playAgain);
+          function playAgain() {
+               $('#win-restart').css("display", "none");
+               location.reload();
+          }
+
+          $('#art-info').click(artInfo);
+          function artInfo() {
+               if  ((((window.matchMedia('(max-width: 768px)').matches)) && ((window.matchMedia( '(orientation: portrait)').matches))) || (((window.matchMedia('(max-width: 1024px)').matches)) && ((window.matchMedia( '(orientation: landscape)').matches))) || (navigator.userAgent.match(/iPad/i) != null)) {
+                    $('#win-restart').css('display', 'none');
+                    $('#modal-slides').css('display', 'block');
+                    $('.display-right, .display-left').hide();
+                    $('#row1, #row2, #row3').hide();
+                    $('.slides-div').show();
+                    $('.modal-content').css({'background-color': 'transparent'});
+                    $('.slides-div').css({'margin-top': '45px'});
+                    $('.button-display-topright').css({'position': 'fixed', 'margin': '20px 0 0 25px', 'font-size': '2.200em'});
                }
                else {
-                    $(innerCard[storedEventData[0]]).toggleClass('flip', false);
-                    $(innerCard[storedEventData[1]]).toggleClass('flip', false); 
-                    storedEventData = [];
-                    x = 0;
-               }    
-          }, 1300);
-     }
-     
-     console.log(storedEventData);
+                    $('#win-restart').css('display', 'none');
+                    $('#modal-slides').css('display', 'block');
+                    var slideIndex = 1;
+               showDivs(slideIndex);
+                    function plusDivs(n) {
+                         showDivs(slideIndex += n);
+                    }
+                    $('.display-left').click(subtractOne);
+                    function subtractOne() {
+                         plusDivs(-1);
+                    }
+                    $('.display-right').click(addOne);
+                    function addOne() {
+                         plusDivs(1);
+                    }
+                    function showDivs(n) {
+                    let x = $('.slides-div');
+                    if (n > x.length) {
+                         slideIndex = 1;
+                    }
+                    if (n < 1) {
+                         slideIndex = x.length;
+                    };
+                    for (let i = 0; i < x.length; i++) {
+                    $(x[i]).css('display', 'none');
+                    }
+                    $(x[slideIndex-1]).css('display', 'contents');
+                    }
+               }
+          }
 
-     $('.button-win-restart-topright').click(closeModal);
-     function closeModal() {
-          $('#win-restart').css("display", "none");
-          location.reload();
-     }
+          $('.button-display-topright').click(closeArtModal);
+          function closeArtModal() {
+               $('#modal-slides').css("display", "none");
+               location.reload();
+          }
 
-     $('#play-again').click(playAgain);
-     function playAgain() {
-          $('#win-restart').css("display", "none");
-          location.reload();
-     }
+          let imageObjectIDS = [551302, 546748, 544222, 543870, 547803, 551502, 549070, 548580, 544864, 570708, 546745]
 
-     $('#art-info').click(artInfo);
-     function artInfo() {
-          $('#win-restart').css('display', 'none');
-          $('#modal-slides').css('display', 'block');
-          var slideIndex = 1;
-     showDivs(slideIndex);
-     function plusDivs(n) {
-          showDivs(slideIndex += n);
-     }
-     $('.display-left').click(subtractOne);
-     function subtractOne() {
-          plusDivs(-1);
-     }
-     $('.display-right').click(addOne);
-     function addOne() {
-          plusDivs(1);
-     }
-     function showDivs(n) {
-     let x = $('.slides-div');
-     if (n > x.length) {
-          slideIndex = 1;
-     }
-     if (n < 1) {
-          slideIndex = x.length;
-     };
-     for (let i = 0; i < x.length; i++) {
-     $(x[i]).css('display', 'none');
-     }
-     $(x[slideIndex-1]).css('display', 'contents');
-     }
-     }
+          let apiURL = 'https://collectionapi.metmuseum.org/public/collection/v1/objects/';
 
-     $('.button-display-topright').click(closeArtModal);
-     function closeArtModal() {
-          $('#modal-slides').css("display", "none");
-          location.reload();
-     }
+          let urls = [];
+          let count = 0;
 
-     let imageObjectIDS = [551302, 546748, 544222, 543870, 547803, 551502, 549070, 548580, 544864, 570708, 546745]
+          imageObjectIDS.forEach(function(i, j){
+               urls[j] = apiURL + i;
+          });
+          console.log(urls)
 
-     let apiURL = 'https://collectionapi.metmuseum.org/public/collection/v1/objects/';
-
-     let urls = [];
-     let count = 0;
-
-     imageObjectIDS.forEach(function(i, j){
-          urls[j] = apiURL + i;
-     });
-     console.log(urls)
-
-     Promise.all(urls.map(url =>
-          fetch(url)
-          .then(convertToJson)
-          .then(useData)
-     )) 
+          Promise.all(urls.map(url =>
+               fetch(url)
+               .then(convertToJson)
+               .then(useData)
+          )) 
 
           function convertToJson(response) {
                //this returns the info so it can be worked with
